92d818de6efda6485c1bed230f4d9835f4b10f02
Guhong <83487617+guhong3648@users.noreply.github.com>
2025-12-29 21:23:48 +0900

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -22,7 +22,7 @@ def plot_emt(emt_data):
     
 '''
 - ZYX Cenvention = [yaw, pitch, roll]
-- After Initialization = [depth, lateral, elevational, pitch, yaw, roll]
+- After Initialization = [depth, lateral, elevational, yaw, pitch, roll]
 - Form = [param (6D Vector), tforms (4x4 Matrix)]
 - y_vec: [B, S, 6] parameter representation
 - a_vec: [B, S, 3] parameter representation of the Euler angle
